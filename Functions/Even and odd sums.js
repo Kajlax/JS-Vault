93772bc5371ee871151ba4628936aff78a45d136
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-10-30 20:15:43 +0200

    Upload

---
+++ Functions/Even and odd sums.js
@@ -0,0 +1,17 @@
+// EVEN & ODD SUMS
+// Return the sums of even and odd numbers in an array
+
+// evenAndOddSums([10, 25, 33, 50, 60, 60, 45, 71]) == [170, 116]
+
+function evenAndOddSums(arr) {
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  arr.forEach(num => (num % 2 === 0 ? (sumEven += num) : (sumOdd += num)));
+  let result = new Array(sumEven, sumOdd);
+
+  return result;
+}
+
+const x = evenAndOddSums([10, 25, 33, 50, 60, 60, 45, 71]);
+console.log(x);
