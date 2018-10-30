47f216ca1a671ffeb6bcec8033f4b0c161bb60bd
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-10-30 19:30:11 +0200

    Upload

---
+++ Functions/Seek and destroy.js
@@ -0,0 +1,9 @@
+// SEEK & DESTROY
+// Pass in two arguments. First the whole array, then the values that should be removed
+
+function seekAndDestroy(arr, ...rest) {
+  return arr.filter(val => !rest.includes(val));
+}
+
+const z = seekAndDestroy([1, 2, 3, 4, 5, 5, 5, "destroy"], 2, 5);
+console.log(z);
