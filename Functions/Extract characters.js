5ecb4d03575fbc815315e95e800c9458f1349d40
Kajlax <32290883+Kajlax@users.noreply.github.com>
2018-11-11 20:56:45 +0200

    Upload

---
+++ Functions/Extract characters.js
@@ -0,0 +1,14 @@
+// EXTRACT CHARCTERS
+// Extract matching characters. Otherwise return no matches.
+
+function extractCharacters(str) {
+  let matched;
+
+  if (str && (matched = str.match(/[aeiou]+/g))) {
+    return matched;
+  }
+  return "No matches";
+}
+
+const x = extractCharacters("Hello world");
+console.log(x);
